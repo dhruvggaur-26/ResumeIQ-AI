3f00493e3944a667bc8d69096df2556f7c02611b
DHRUV-GAUR26 <dhruvgaue2606@gmail.com>
2026-06-22 20:57:25 +0530

    Fix CORS for Vercel frontend

--- ai-service/main.py
+++ ai-service/main.py
@@ -55,7 +55,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "https://resumeiq-ai-backend.onrender.com"
+    "https://resume-iq-ai-ecru.vercel.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],
